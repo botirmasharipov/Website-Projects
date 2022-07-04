4786fd4889e4c68243449763101d80907f078251
botirmasharipov <masbotirjon963@gmail.com>
2022-07-04 13:08:40 -0700

    editPlayList

--- Playlist/app.js
+++ Playlist/app.js
@@ -1,5 +1,8 @@
-// let container = document.querySelector("button");
-// let button = container.querySelectorAll(
-//   ".form__submit-btn form__submit-btn_action_add"
-// );
-
+let container = document.querySelector(".container")
+let songsContainer = container.querySelector(".songs-container")
+let songs = songsContainer.querySelectorAll(".song")
+let addButton = container.querySelector(".form__submit-btn_action_add")
+let resetButton = container.querySelector(".form__submit-btn_action_reset")
+if (songs.length === 0){
+  resetButton.setAttribute("disabled", true);
+  resetButton.classList.add("form__submit-btn_disabled")
